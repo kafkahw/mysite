b667e0d99f987a277c789818e3eed4386e9bda2a
Wei Hou <weihou.usc@gmail.com>
2013-11-27 16:19:49 -0600

    removing 'south' from installed_apps

--- mysite/settings.py
+++ mysite/settings.py
@@ -126,7 +126,7 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
     'polls',
-    'south',
+    # 'south',
 )
 
 # A sample logging configuration. The only tangible logging
